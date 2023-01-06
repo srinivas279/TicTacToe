939f56253a977d2743ddf22d820d1aa59b0cfb65
srinivas_bolem <srinivasbolem1997@gmail.com>
2023-01-06 11:50:44 +0530

    Created board and square

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Board from "./components/Board"
 
  const App = () => (
   <>
     <h1>Welcome to React Vite Micro App!</h1>
     <p>hello Srinivas.</p>
+    <Board/>
   </>
 );
 
---
+++ src/components/Square.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Square = (props) => {
+  return (
+    <button type="button">{props.value}</button>
+  )
+}
+
+export default Square
